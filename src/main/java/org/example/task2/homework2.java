10389cb21a3ee3854e7a6fd13f6e06c762672eab
Viktor Irchits <repetitorbel@gmail.com>
2025-09-14 10:10:01 +0300

    Homework #2 (only screenshots)

---
+++ src/main/java/org/example/task2/homework2.java
@@ -0,0 +1,13 @@
+package org.example.task2;
+
+public class homework2 {
+    public static void main(String[] args) {
+        // Скриншоты работы с git через командную строку:
+        // 1. screenshot1.png - создание отдельной ветки и проверка
+        // 2. screenshot2.png - Проверка статуса репозитория
+        // 3. screenshot3.png - Коммит
+        // 4. screenshot4.png, screenshot5.png - Пушинг в удаленный репозиторий
+
+        System.out.println("Скриншоты находятся в папке проекта");
+    }
+}
